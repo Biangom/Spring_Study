dc899de1c32896092e195872bc197aff7d83e7ae
김성식 <kss7858@naver.com>
2019-04-22 18:03:29 +0900

    Revert "Today'Study :"
    This reverts commit fb6d69c31d80ac2cff2d8362b0e2fbe5de6b991a.

--- DITest/src/main/java/di6/ChinaTire.java
+++
@@ -1,7 +0,0 @@
-package di6;
-
-public class ChinaTire implements Tire{
-	public String getName() {
-		return "중국산 타이어";
-	}
-}
--- DITest/src/main/java/di6/KoreaTire.java
+++
@@ -1,7 +0,0 @@
-package di6;
-
-public class KoreaTire implements Tire{
-	public String getName() {
-		return "국산 타이어";
-	}
-}
